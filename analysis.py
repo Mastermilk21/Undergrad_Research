f5fa5a557dc68a564a52b0d6fc34030440f08912
Eitan Klarich <eklarich@ttu.edu>
2024-05-08 13:27:51 -0500

    object, lombscargel

--- analysis.py
+++ analysis.py
@@ -1,8 +1,8 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-#from astropy.timeseries import LombScargle
-#from scipy.signal import find_peaks
+from astropy.timeseries import LombScargle
+from scipy.signal import find_peaks
 
 
 
@@ -21,6 +21,27 @@ class Lightcurve:
         plt.grid(True)
         plt.show()
 
-    def lombscargle(self, )
+    def lombscargle(self, cadence):
+        data = np.loadtxt(self.file_path)
+        self.time = data[:, 1]  
+        self.magnitude = data[:, 4] 
+        ls = LombScargle(self.time, self.magnitude)
+        self.cadence = cadence
+        try:
+            cadence_hours = float(cadence)
+            if cadence_hours <= 0:
+                raise ValueError("Invalid cadence value. Cadence must be greater than 0.")
+        except ValueError:
+            print("Invalid input for cadence. Please enter a valid numeric value.")
+            return
+        max_frequency = 1 / cadence_hours
+        frequency = np.linspace(0.01, max_frequency, 10000)
+        power = ls.power(frequency, normalization='standard')  
+        period = 24 / frequency 
+        print("the power is", power, "the period os", period)
+
+    def print_plot(self):
+        
+         
 
     
