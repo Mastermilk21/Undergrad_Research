7c20e6534847fab10cb6c3881f946affab71d0cf
Eitan Klarich <eklarich@ttu.edu>
2024-05-31 17:17:11 -0500

    accesess the boundry object

---
+++ analyzer.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from boundry_analysis import BoundryLightCurve
+
+filename = r"c:\Users\eitan\code repos\data\extracted tarballs\sector54\lc_2022pix_cleaned.txt"
+
+x_min = 2770
+x_max = 2773
+cadence = 0.5
+analyze = BoundryLightCurve(filename, x_min=x_min, x_max=x_max)
+analyze.plot_lightcurve()
+analyze.lombscargle(cadence)
+#analyze.plot_lombscargle()
+#analyze.plot_combined()
+analyze.phasefold()
