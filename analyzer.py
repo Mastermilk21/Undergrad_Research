5c9f37394facc4fd789005e023d537899381af25
Eitan Klarich <eklarich@ttu.edu>
2024-06-05 10:31:34 -0500

    call code for boundry_analysis

--- analyzer.py
+++ analyzer.py
@@ -5,12 +5,12 @@ from boundry_analysis import BoundryLightCurve
 
 filename = r"c:\Users\eitan\code repos\data\extracted tarballs\sector54\lc_2022pix_cleaned.txt"
 
-x_min = 2770
-x_max = 2773
+x_min = 2784.5
+x_max = 2785
 cadence = 0.5
 analyze = BoundryLightCurve(filename, x_min=x_min, x_max=x_max)
 analyze.plot_lightcurve()
 analyze.lombscargle(cadence)
-#analyze.plot_lombscargle()
-#analyze.plot_combined()
+analyze.plot_lombscargle()
+analyze.plot_combined()
 analyze.phasefold()
