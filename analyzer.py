272c585543d69f9d02668c4760f6bf94f302625e
Eitan Klarich <eklarich@ttu.edu>
2025-06-25 13:26:42 -0500

    added sector identifier

--- analyzer.py
+++ analyzer.py
@@ -3,14 +3,19 @@ import matplotlib.pyplot as plt
 
 from boundry_analysis import BoundryLightCurve
 
-filename = r"c:\Users\eitan\code repos\data\extracted tarballs\sector28\lc_2020rtt_cleaned.txt"
+sector = "65"
+object_name = "2023iwl"
 
-x_min = 2075    
-x_max = 2078
+filename = f"c:/Users/eitan/code repos/data/extracted tarballs/sector{sector}/lc_{object_name}_cleaned.txt"
+
+
+x_min = 3069.6
+x_max = 3074
 height = 0.015
 analyze = BoundryLightCurve(filename, x_min=x_min, x_max=x_max, height=height)
 analyze.plot_lightcurve()
-analyze.plot_combined()
+analyze.newfastplot()
+#analyze.plot_combined()
 #analyze.phasefold()
-analyze.plotposter()
+#analyze.plotposter()
 #analyze.bounded_lightcurve()
